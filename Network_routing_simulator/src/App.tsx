3ef733266640a5bb8e996526a2ff58ff9e7a6cbe
nirajan1111 <nirajansah1111@gmail.com>
2024-01-30 14:35:38 +0545

    initial

---
+++ Network_routing_simulator/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <h1 className="text-3xl text-gray-300 bg-black font-bold underline">
+      Let's build
+    </h1>
+  );
+}
+
+export default App;
